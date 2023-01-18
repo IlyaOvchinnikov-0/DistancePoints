47dfc3462fea9c8bb190ef127111b642aa60cc64
IlyaOvchinnikov-0 <ilyaovchinnokov4@gmail.com>
2023-01-18 16:21:32 +0400

    first commit

---
+++ DistancePoints/Point.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DistancePoints
+{
+    class Point
+    {
+        double x;
+        double y;
+
+        public static Point CreatePoint(double x, double y)
+        {
+            var point = new Point();
+            point.x = x;
+            point.y = y;
+
+            return point;
+        }
+
+        public double Distance(Point somePoint)
+        {
+            double x2 = this.x + somePoint.x;
+            double y2 = this.y + somePoint.y;
+
+            return Math.Sqrt(x2 * x2 + y2 * y2);
+        }
+    }
+}
---
+++ DistancePoints/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DistancePoints
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var p1 = Point.CreatePoint(1, 5);
+            var p2 = Point.CreatePoint(1, 2);
+
+            Console.WriteLine(p1.Distance(p2));
+            Console.ReadKey();
+        }
+    }
+}
